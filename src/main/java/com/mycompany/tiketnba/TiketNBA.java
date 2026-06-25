9266a19da8449bdd360216e6cba104f8534c0b5b
Frizam <frizammaulana07@gmail.com>
2026-06-25 20:45:27 +0700

    Update TiketNBA.java

--- src/main/java/com/mycompany/tiketnba/TiketNBA.java
+++ src/main/java/com/mycompany/tiketnba/TiketNBA.java
@@ -21,7 +21,7 @@ public class TiketNBA {
             System.out.println("3. Sort Games by Price");
             System.out.println("4. Book a Ticket");
             System.out.println("5. View Booking History");
-            System.out.println("6. Exit ");
+            System.out.println("6. Exit");
             System.out.print("Choose an option: ");
 
             String choice = scanner.nextLine();
